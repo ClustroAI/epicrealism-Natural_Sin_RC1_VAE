bc3e11df63c086da80257753cd301aded3970bac
totoleon <hailong.leon@gmail.com>
2023-11-11 14:58:07 -0800

    Accept simple prompt as input

--- model_invoke.py
+++ model_invoke.py
@@ -11,10 +11,15 @@ pipe = pipe.to("cuda")
 pipe.safety_checker = None
 
 def invoke(input_text):
-    input_json = json.loads(input_text)
-    prompt = input_json['prompt']
-    negative_prompt = input_json['negative_prompt'] if 'negative_prompt' in input_json else ""
-    steps = int(input_json['steps']) if 'steps' in input_json else 50
+    try:
+        input_json = json.loads(input_text)
+        prompt = input_json['prompt']
+        negative_prompt = input_json.get('negative_prompt', "")
+        steps = int(input_json.get('steps', 50))
+    except:
+        prompt = input_text
+        negative_prompt = ""
+        steps = 50
     image = pipe(prompt=prompt, negative_prompt=negative_prompt, num_inference_steps=steps).images[0]
     image.save("generated_image.png")
     return "generated_image.png"
